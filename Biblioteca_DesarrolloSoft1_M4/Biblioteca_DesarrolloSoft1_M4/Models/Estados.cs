da56e840f464a7b486baad2dee99d6904f83a9e4
carro-dev <carlosnoelluciano@hotmail.com>
2023-01-08 03:02:20 -0400

    Añadiendo Proy Wpf C#

---
+++ Biblioteca_DesarrolloSoft1_M4/Biblioteca_DesarrolloSoft1_M4/Models/Estados.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_DesarrolloSoft1_M4.Models
+{
+    public class Estados
+    {
+        public int id_estado { get; set; }
+        public string estado { get; set; }
+    }
+}
---
+++ Biblioteca_DesarrolloSoft1_M4/Biblioteca_DesarrolloSoft1_M4/Models/Rol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_DesarrolloSoft1_M4.Models
+{
+    public class Rol
+    {
+        public int id_rol { get; set; }
+        public string rol { get; set; }
+    }
+}
