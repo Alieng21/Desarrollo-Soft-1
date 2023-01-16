006e5647f8b0b8dac72f92eb7f4261bf2f30bfdb
Alieng21 <alianygonzalez21@gmail.com>
2023-01-16 05:32:14 -0400

    commit

---
+++ Biblioteca_DesarrolloSoft1_M4/Biblioteca_DesarrolloSoft1_M4/Vistas/bibliotecario/BibliotecarioPrestamo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Biblioteca_DesarrolloSoft1_M4.Vistas.bibliotecario
+{
+    /// <summary>
+    /// Lógica de interacción para BibliotecarioPrestamo.xaml
+    /// </summary>
+    public partial class BibliotecarioPrestamo : Window
+    {
+        public BibliotecarioPrestamo()
+        {
+            InitializeComponent();
+        }
+    }
+}
